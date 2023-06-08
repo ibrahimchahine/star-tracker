3022d68280b2f323930bd2c757d3d84c7a01a373
root <root@FriendlyELEC>
2023-06-09 05:38:24 +0800

    added images

--- NanoPi/snapshot.py
+++ NanoPi/snapshot.py
@@ -3,7 +3,8 @@ import os
 
 snapshot_call = "wget http://192.168.51.149:8080/?action=snapshot -O"
 
-n = 1
+n = 10
+
 for i in range(n):
     call = os.system(snapshot_call + " images/" + str(i) + "_2output.jpg")
 
